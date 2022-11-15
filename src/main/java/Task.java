0da190f80fdd3cba979eed445f991299944527ee
uoft-flyfreejay <jaehyung.lee@mail.utoronto.ca>
2022-11-15 17:58:29 -0500

    fixed Task class and changed the variables protected

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,10 @@
 public abstract class Task {
     protected boolean completed = false;
     protected boolean activated = false;
-    public abstract void setCompletedStatus(boolean completed);
+    public abstract void setCompletionStatus(boolean completed);
 
-    public abstract boolean getCompletedStatus();
+    public abstract boolean getCompletionStatus();
 
     public abstract void setActivatedStatus(boolean activated);
+    public abstract boolean getActivatedStatus();
 }
