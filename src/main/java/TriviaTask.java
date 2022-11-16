42c2cd357d6f640fe9ecdf1d35065c3ad51ca078
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-16 18:55:46 -0500

    trivia class created i can't get it to extend task yet

---
+++ src/main/java/TriviaTask.java
@@ -0,0 +1,26 @@
+public class TriviaTask extends Task{
+    private boolean completed = false;
+    private String current_question;
+    private String player_answer;
+    private String actual_answer;
+
+//    @Override
+//    public void setCompletedStatus(boolean completed) {
+//        // this is where you set whether or not the trivia task is complete
+//        // trivia task will be complete if ...
+//
+//    }
+//    @Override
+//    public boolean getCompletedStatus() {
+//        // determine whether or not the task is completed
+//        // in the context of the trivia task, this is where you find out if the trivia task has been completed
+//        return this.completed;
+//
+//    }
+//
+//    @Override
+//    public void display() {
+//        // do not call an outer layer here
+
+    }
+}
