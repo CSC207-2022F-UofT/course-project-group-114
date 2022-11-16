9e847c510d01a5a1ec11339255dec8d711c7a4e8
elenascobici <elena@scobici.com>
2022-11-16 17:08:26 -0500

    Created HeatAdjustmentTask class, updated GameMaster display method to reset

---
+++ src/main/java/HeatAdjustmentTask.java
@@ -0,0 +1,7 @@
+public class HeatAdjustmentTask {
+    public int actualTemp; // The actual temperature the user must achieve
+    public int currentTemp;
+    public HeatAdjustmentTask() {
+
+    }
+}
