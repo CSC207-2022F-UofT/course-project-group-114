b4bc48f747bb5379f74822cf8ce45bdad563efa4
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-16 16:16:03 -0500

    fixed minor index issues

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -5,7 +5,6 @@ public class Menu {
     // this class controls the backend of the menu
     /* should have functions that do:
         shows scores
-        start a game master..?
      */
 
     // we need to agree to a path to store the file for saving
@@ -13,13 +12,13 @@ public class Menu {
     public static String[] getScores() throws FileNotFoundException // should be triggered after highscore screen open is clicked
     {
         Scanner scan = new Scanner(new File("users.csv")); // goes to the path to find the file
-        scan.useDelimiter(",");
+        
         int index = 0;
         String line;
         String[] scores = new String[7];
-        while(index <= 7){
+        while(index < 7){
             line = scan.next();  // reads the line
-            scores[index] = line.split(",")[4]; // extracts the highscore and adds to array
+            scores[index] = line.split(",")[3]; // extracts the highscore and adds to array
             index += 1;
         }
         
