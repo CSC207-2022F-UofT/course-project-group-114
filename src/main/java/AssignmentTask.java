e2d33fadda9f49fd04fc60555ccb4c1763904664
uoft-flyfreejay <jaehyung.lee@mail.utoronto.ca>
2022-11-16 15:15:51 -0500

    finished AssignmentTask and relevant class methods. Also repushing Task abstract class since changes got overwritten

--- src/main/java/AssignmentTask.java
+++ src/main/java/AssignmentTask.java
@@ -1,27 +1,51 @@
 public class AssignmentTask extends Task{
-    private boolean completed = false;
-    private int characterCountNeeded;
-    private int currentCharacterCount;
+    protected boolean completed = false;
+    protected int characterCountNeeded;
+    protected int currentCharacterCount;
 
     public AssignmentTask(){
-        this.completed = true;
-        this.characterCountNeeded = 0;
+        this.completed = false;
+        setCharacterCountNeeded();
         this.currentCharacterCount = 0;
     }
 
+    public void setCharacterCountNeeded(){
+        // generate random number from lowerbound and upperbound
+        int COUNT_UPPERBOUND = 50;
+        int COUNT_LOWERBOUND = 30;
+        this.characterCountNeeded =
+                (int) Math.floor(Math.random()*(COUNT_UPPERBOUND - COUNT_LOWERBOUND +1)+ COUNT_LOWERBOUND);
+    }
+
+    public void setCurrentCharacterCount(int currCharCount){
+        this.currentCharacterCount = currCharCount;
+    }
+
+    public void checkCount(){
+        if (this.currentCharacterCount >= this.characterCountNeeded){
+            this.setCompletionStatus(true);
+            this.setActivatedStatus(false);
+        }
+    }
     @Override
-    public void setCompletedStatus(boolean completed) {
+    public void setCompletionStatus(boolean completed) {
         this.completed = completed;
     }
 
     @Override
-    public void getCompletedStatus() {
+    public boolean getCompletionStatus() {
         return this.completed;
     }
 
+
     @Override
     public void setActivatedStatus(boolean activated) {
+        this.activated = activated;
+    }
 
+    @Override
+    public boolean getActivatedStatus() {
+        return this.activated;
     }
 
 
