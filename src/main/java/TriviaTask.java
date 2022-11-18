de2d3ec2683067e763e4e2804558f107fbd9105a
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-17 19:52:49 -0500

    starting to write pseudocode and comments

--- src/main/java/TriviaTask.java
+++ src/main/java/TriviaTask.java
@@ -1,4 +1,9 @@
-package src.main.java;
-
 public class TriviaTask extends Task{
+    // the TriviaTask subclass of Task
+    // TriviaTask use case of our game!!
+
+    @Override
+    public void reset() {
+
+    }
 }
