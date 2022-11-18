7418e4d20e40a86162dc18eb63e11cfb9dfcc140
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-17 19:54:37 -0500

    starting to write pseudocode and comments

--- src/main/java/TriviaTask.java
+++ src/main/java/TriviaTask.java
@@ -2,6 +2,15 @@ public class TriviaTask extends Task{
     // the TriviaTask subclass of Task
     // TriviaTask use case of our game!!
 
+    // the TriviaTask class is the backend of the trivia game!
+    // it extends Task
+
+    // is called by TriviaTask controller
+
+    // checks the player's answer to the question
+    // if the answer is correct
+    // if the answer is incorrect
+
     @Override
     public void reset() {
 
