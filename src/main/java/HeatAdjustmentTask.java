93e31d6c1b2a2a3d6962b24d4dd33bf0a74d62ca
elenascobici <elena@scobici.com>
2022-11-19 14:25:55 -0500

    Made HeatAdjustmentTask methods static

--- src/main/java/HeatAdjustmentTask.java
+++ src/main/java/HeatAdjustmentTask.java
@@ -1,22 +1,23 @@
 import java.util.Random;
 public class HeatAdjustmentTask extends Task {
-    public int answerTemp; // The actual temperature the user must achieve
-    public int currentTemp; // The user's current temperature
+    public static int answerTemp; // The actual temperature the user must achieve
+    public static int currentTemp; // The user's current temperature
     public HeatAdjustmentTask() {
-        this.reset();
+        reset();
     }
-    public void reset() { // Inherit reset method; generate random answer and current temperatures
-        this.answerTemp = this.randomizeCurrentTemp();
-        this.currentTemp = this.randomizeCurrentTemp();
+
+    public static void reset() { // Inherit reset method; generate random answer and current temperatures
+        answerTemp = randomizeCurrentTemp();
+        currentTemp = randomizeCurrentTemp();
     }
-    private int randomizeCurrentTemp() { // Return a random integer between 0 and 30
+    private static int randomizeCurrentTemp() { // Return a random integer between 0 and 30
         Random rand = new Random();
         return rand.nextInt(30);
     }
-    public void tempUp() { // Method to be called when up button is pressed
-        this.currentTemp++;
+    public static void tempUp() { // Method to be called when up button is pressed
+        currentTemp++;
     }
-    public void tempDown() { // Method to be called when down button is pressed
-        this.currentTemp--;
+    public static void tempDown() { // Method to be called when down button is pressed
+        currentTemp--;
     }
 }
