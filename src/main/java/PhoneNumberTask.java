84e9b725749816b87fcadc628dcb1970255425a5
kaavyakalani26 <kaavya.kalani@mail.utoronto.ca>
2022-11-19 02:01:04 -0500

    Added resources for the PhoneNumberTaskView.java, completed the view for the Phone Number Task. Made the controller and use case also but need to look over it again to confirm functionality with other classes and ensure Clean Architecture.

--- src/main/java/PhoneNumberTask.java
+++ src/main/java/PhoneNumberTask.java
@@ -1,21 +1,16 @@
-public class PhoneNumberTask extends Task {
-    public int actual_number;
-    public int pressed_number;
+public class PhoneNumberTask extends Task{
 
-    public PhoneNumberTask(int actual, int pressed){
-        this.actual_number = actual;
-        this.pressed_number = pressed;
-    }
-
-    public void check_correctness(int actual_number, int pressed_number){
+    public boolean check_correctness(int actual_number, int pressed_number){
         if(actual_number == pressed_number){
-            this.setCompletedStatus(true);
-            GameMaster.tasksCompletedCount ++;
-            this.setActivationStatus(false);
+            setCompletionStatus(true);
+            lifeMaster.incrementTaskCount();
+            setActivatedStatus(false);
+            return true;
         } else {
             lifeMaster.deductLife();
-            this.setActivationStatus(false);
+            setActivatedStatus(false);
+            return false;
         }
     }
-
 }
+
