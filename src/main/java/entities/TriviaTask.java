099b4f728be1a5745c579c8dbdcd0159198985e9
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-20 14:40:28 -0500

    11/20 2pm

--- src/main/java/entities/TriviaTask.java
+++ src/main/java/entities/TriviaTask.java
@@ -1,15 +1,16 @@
 package entities;
 
 public class TriviaTask extends Task {
-    public int triviaCategory;
+//    public int triviaCategory; can later make changes to code to add categories and get questions and answers from a csv file
+    public int questionNum;
     public String currentQuestion;
     public String playerAnswer;
     public String actualAnswer;
 
 
-    Hashmap<String, ArrayList<String>> category0 = new HashMap<String, List<String>>();
-    category0.put("What is the missing word: S***D Design Principles?"), Arrays.asList("solid");
-    category0.put("Who wrote clean architecture?"), Arrays.asList("uncle bob", "real name");
+//    Hashmap<String, ArrayList<String>> category0 = new HashMap<String, List<String>>();
+//    category0.put("What is the missing word: S***D Design Principles?"), Arrays.asList("solid");
+//    category0.put("Who wrote clean architecture?"), Arrays.asList("uncle bob", "real name");
 
 
 
@@ -29,15 +30,17 @@ public class TriviaTask extends Task {
     // if the answer is incorrect
 
     @Override
-    public void reset() { // TriviaTask inherited reset method from its parent class.
-        // choose the question aka current_question
-        // get the actual_answer of the corresponding question
+    public void reset() { // inherited reset method, generate random question
+        // public int questionNum = random number between 0 and last index of dictionary
+        //
     }
     public boolean checkAnswer (String actualAnswer, String playerAnswer){
-        if (playerAnswer.equals(actualAnswer)){
+        if (playerAnswer.toLowerCase().equals(actualAnswer.toLowerCase())){
             setCompletionStatus(true);
             return true;
         }
-        return false;
+        else{
+            return false;
+        }
     }
 }
