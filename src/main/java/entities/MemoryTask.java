65d52e03e069401ca3e0309851362cd3ccbfdbae
taliafabs <113137476+taliafabs@users.noreply.github.com>
2022-11-20 14:50:17 -0500

    Update MemoryTask.java

--- src/main/java/entities/MemoryTask.java
+++ src/main/java/entities/MemoryTask.java
@@ -3,7 +3,7 @@ package entities;
 public class MemoryTask extends Task{
 
     public boolean check_correctness(String actual_password, String typed_password){
-        if(actual_password.equals(typed_password)){
+        if(typed_password.equals(actual_password)){
             setCompletionStatus(true);
             return true;
         }
