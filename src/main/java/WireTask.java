89e3055708f99b18843649fcb718fb9d9386c288
Roy Mao <roy.mao@mail.utoronto.ca>
2022-11-19 23:50:46 -0500

    Implementation for WireTask

---
+++ src/main/java/WireTask.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class WireTask extends Task{
+
+    private static ArrayList<String> LeftWires;
+    // The list containing strings of the colours of the wires on the left side
+
+    private static ArrayList<String> RightWires;
+    // The list containing strings of the colours of the wires on the right side
+
+    private static String previous;
+    // The node that the user clicked before currentClick. If the user did not click anything or only clicked once
+    // , then previous is set to Null.
+
+    private static String current;
+    // The node that the user last clicked. If the user did not click anything, then current is Null.
+
+
+    public static ArrayList<String> setLeftWires(){
+        // Shuffles a random list from LeftWires for the left side
+        if (!LeftWires.contains("RED")){
+            LeftWires.add("RED");
+        }
+        if (!LeftWires.contains("GREEN")){
+            LeftWires.add("GREEN");
+        }
+        if (!LeftWires.contains("BLUE")){
+            LeftWires.add("BLUE");
+        }
+        Collections.shuffle(LeftWires);
+        return LeftWires;
+    }
+
+    public static ArrayList<String> setRightWires(){
+        // Shuffles a random list from RightWires for the right side
+        if (!RightWires.contains("RED")){
+            RightWires.add("RED");
+        }
+        if (!RightWires.contains("GREEN")){
+            RightWires.add("GREEN");
+        }
+        if (!LeftWires.contains("BLUE")){
+            RightWires.add("BLUE");
+        }
+        Collections.shuffle(RightWires);
+        return RightWires;
+    }
+
+    public static void checkWiresSelected() {
+        // Checks whether the two last wires the user selected are matching. If so, remove this colour from the list,
+        // and add to the completed list of wires.
+        if (current.equals(previous)) {
+            LeftWires.remove(current);
+            RightWires.remove(current);
+        }
+    }
+
+    public static void checkCompletion(){
+        if (LeftWires.isEmpty()){
+            setCompletionStatus(true);
+            setActivatedStatus(false);
+        }
+    }
+}
