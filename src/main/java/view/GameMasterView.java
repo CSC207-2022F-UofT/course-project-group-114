5942c6cbcac88012c8f5530cbb99e005bb9d57a0
elenascobici <elena@scobici.com>
2022-11-20 00:15:33 -0500

    Created basic GameMaster view with background and clock

--- src/main/java/view/GameMasterView.java
+++ src/main/java/view/GameMasterView.java
@@ -5,4 +5,45 @@ import javax.swing.*;
 import controller.GameMasterController;
 
 public class GameMasterView extends JFrame{
+    JLayeredPane layers;
+    ImageIcon backgroundIcon;
+    JLabel background;
+    ImageIcon catClockIcon;
+    JLabel catClock;
+
+    public GameMasterView() {
+        layers = new JLayeredPane();
+
+        backgroundIcon = scaleIcon("src\\main\\java\\resources\\GameMaster\\background.jpg", 1280, 720);
+        background = new JLabel();
+        background.setBounds(0, 0, 1280, 720);
+        background.setIcon(backgroundIcon);
+
+//        catClockIcon = scaleIcon("src\\main\\java\\resources\\GameMaster\\clock\\catclock2.png", 200, 100);
+        catClockIcon = new ImageIcon("src\\main\\java\\resources\\GameMaster\\clock\\catclock.gif");
+        catClock = new JLabel();
+        catClock.setBounds(0, 0, 1280, 720);
+        catClock.setIcon(catClockIcon);
+
+        layers.add(background, Integer.valueOf(0));
+        layers.add(catClock, Integer.valueOf(1));
+        layers.setPreferredSize(new Dimension(1280, 720));
+
+        add(layers);
+
+        setSize(1280,720);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        layers.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameMasterView();
+    }
+
+    private ImageIcon scaleIcon(String iconSource, int w, int h) {
+        ImageIcon icon = new ImageIcon(iconSource);
+        return new ImageIcon(icon.getImage().getScaledInstance(w, h, Image.SCALE_SMOOTH));
+    }
 }
