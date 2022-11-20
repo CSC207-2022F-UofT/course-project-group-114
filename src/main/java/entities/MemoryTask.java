125d7aec0adeaca437238c0960bb0eab140a1a61
taliafabs <113137476+taliafabs@users.noreply.github.com>
2022-11-20 14:51:11 -0500

    Update MemoryTask.java

--- src/main/java/entities/MemoryTask.java
+++ src/main/java/entities/MemoryTask.java
@@ -1,7 +1,10 @@
 package entities;
 
-public class MemoryTask extends Task{
-
+public class MemoryTask extends Task{   // memory task backend code
+    
+   
+    // checks the correctness of the password entered by the user.
+    // typed password relayed here from view by the controller
     public boolean check_correctness(String actual_password, String typed_password){
         if(typed_password.equals(actual_password)){
             setCompletionStatus(true);
