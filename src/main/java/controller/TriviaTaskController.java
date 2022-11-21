ec3f4e86898c735306ad5842d395386949282e3c
taliafabs <113137476+taliafabs@users.noreply.github.com>
2022-11-21 13:52:53 -0500

    Update TriviaTaskController.java

--- src/main/java/controller/TriviaTaskController.java
+++ src/main/java/controller/TriviaTaskController.java
@@ -2,10 +2,18 @@ package controller;
 import entities.TriviaTask;
 
 public class TriviaTaskController {
-    TriviaTask usecase = new TriviaTask();
+    TriviaTask TriviaTaskUsecase = new TriviaTask();
 
     // public int passCategory (JTextField chosen)
     // if it's ... return respective integer
+    // can add this in later, not a priority
+    public String passRandomQuestion(){
+        return TriviaTaskUsecase.getRandomQuestion();
+    }
 
-    // public boolean passAnswer
+    // public boolean to check the answer
+    // backend checkAnswer compares the player's answer to the actual answer to determine if it's correct
+    public boolean passAnswer(String actualAnswer, JTextField playerAnswer){
+        return (TriviaTaskUsecase.checkAnswer(actualAnswer, playerAnswer);
+                }
 }
