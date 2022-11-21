ebe55be85ad0384c46a707e676119c0eef1631c9
kaavyakalani26 <kaavya.kalani@mail.utoronto.ca>
2022-11-21 16:19:57 -0500

    Made method in the backend class static

--- src/main/java/controller/MemoryTaskController.java
+++ src/main/java/controller/MemoryTaskController.java
@@ -5,10 +5,9 @@ import entities.MemoryTask;
 import javax.swing.*;
 
 public class MemoryTaskController {
-    MemoryTask usecase = new MemoryTask();
 
     public boolean passer(String actual_password, JTextField typed_password){
-        return (usecase.check_correctness(actual_password, typed_password.getText()));
+        return (MemoryTask.check_correctness(actual_password, typed_password.getText()));
     }
 
 }
--- src/main/java/entities/MemoryTask.java
+++ src/main/java/entities/MemoryTask.java
@@ -5,7 +5,7 @@ public class MemoryTask extends Task{   // memory task backend code
    
     // checks the correctness of the password entered by the user.
     // typed password relayed here from view by the controller
-    public boolean check_correctness(String actual_password, String typed_password){
+    public static boolean check_correctness(String actual_password, String typed_password){
         if(typed_password.equals(actual_password)){
             setCompletionStatus(true);
             return true;
