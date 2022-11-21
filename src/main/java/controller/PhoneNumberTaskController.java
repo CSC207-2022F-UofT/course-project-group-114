3628431d07bbd21b3f4cf55651764ae03d245546
kaavyakalani26 <kaavya.kalani@mail.utoronto.ca>
2022-11-21 16:13:34 -0500

    Made method in the backend class static

--- src/main/java/controller/PhoneNumberTaskController.java
+++ src/main/java/controller/PhoneNumberTaskController.java
@@ -1,15 +1,14 @@
 package controller;
 import entities.PhoneNumberTask;
 public class PhoneNumberTaskController {
-    PhoneNumberTask usecase = new PhoneNumberTask();
 
     public boolean passer(int actual_number, String pressed_number){
         if(pressed_number.length() > 9){
             int input_number = Integer.parseInt(pressed_number.substring(0,9));
-            return (usecase.check_correctness(actual_number, input_number));
+            return (PhoneNumberTask.check_correctness(actual_number, input_number));
         }
         else{
-            return(usecase.check_correctness(actual_number, Integer.parseInt(pressed_number)));
+            return(PhoneNumberTask.check_correctness(actual_number, Integer.parseInt(pressed_number)));
         }
     }
 }
--- src/main/java/entities/PhoneNumberTask.java
+++ src/main/java/entities/PhoneNumberTask.java
@@ -2,7 +2,7 @@ package entities;
 
 public class PhoneNumberTask extends Task {
 
-    public boolean check_correctness(int actual_number, int pressed_number){
+    public static boolean check_correctness(int actual_number, int pressed_number){
         if(actual_number == pressed_number){
             setCompletionStatus(true);
             return true;
