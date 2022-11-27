8ae7277bb23fcf0f2365a8d2afd788ea87e2445a
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-27 14:53:51 -0500

    trivia task view progress working on submit button and passing the player's answer to the controller

--- src/main/java/view/TriviaTaskView.java
+++ src/main/java/view/TriviaTaskView.java
@@ -13,7 +13,7 @@ public class TriviaTaskView extends JFrame {
 
     public static JLayeredPane TriviaTaskPanel;
 
-    TriviaTaskView(){
+    TriviaTaskView() {
 
         TriviaTaskController controller = new TriviaTaskController();
         String currentQuestion = controller.passRandomQuestion();
@@ -29,6 +29,12 @@ public class TriviaTaskView extends JFrame {
 
         TriviaTaskPanel = new JLayeredPane();
         TriviaTaskPanel.setPreferredSize(new Dimension(1280, 720));
+
+            // setting up submit button
+        ImageIcon submit = new ImageIcon("src\\main\\java\\resources\\MemoryTask\\login.png");
+        JButton submitButton = new JButton(submit);
+        submitButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        submitButton.setBounds(200, 580, submit.getIconWidth(), submit.getIconHeight());
 
         // setting up player input for their answer
         // will be sent into the controller to be sent to backend to be checked
@@ -46,26 +52,43 @@ public class TriviaTaskView extends JFrame {
         setPreferredSize(new Dimension(1280, 720));
         setVisible(true);
 
-        javax.swing.Timer timer = new Timer(5000, null);
-        timer.setRepeats(false);
-        timer.start();
-        add(popup);
-        while(timer.isRunning()){
-            popup.setVisible(true);
-            // set the triviaTaskPanel visible to True
-        }
-        timer.stop();
+//        javax.swing.Timer timer = new Timer(5000, null);
+//        timer.setRepeats(false);
+//        timer.start();
+//        add(popup);
+//        while(timer.isRunning()){
+//            popup.setVisible(true);
+//            // set the triviaTaskPanel visible to True
+//        }
+//        timer.stop();
         add(TriviaTaskPanel);
         TriviaTaskPanel.setVisible(true);
         popup.setVisible(true);
 
         // add submit button
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // pass the playerAnswer to the controller
+                //boolean correct = controller.passAnswer(playerAnswer);
+                // if it is correct
+                // show a message that the answer is correct
+
+                // if not, show message that answer is incorrect
+
+            setVisible(false);
+            GameMasterView.backToMain(GameMasterView.triviaTaskView);
+            }
+        });
+        {
+
+        }
 
 
 
-    }
 
-    // ignore this for now. can be added later once trivia is up and running
+
+        // ignore this for now. can be added later once trivia is up and running
 //    public JPanel chooseCategoryPopup;
 //    public JButton submitAnswerButton;
 //
@@ -81,8 +104,9 @@ public class TriviaTaskView extends JFrame {
 //    chooseCategoryPopup.add(chooseMessage);
 
 
-    // display the question & answer box and give player time to answer
+        // display the question & answer box and give player time to answer
 
-    // the playerAnswer is the answer that the player has submitted. controller will have to relay this to the view
+        // the playerAnswer is the answer that the player has submitted. controller will have to relay this to the view
 
+    }
 }
