4f0f749921c40295a1b9a888f0edcb1b915503d1
talia <113137476+taliafabs@users.noreply.github.com>
2022-11-30 18:58:01 -0500

    updates to the trivia task controller

--- src/main/java/controller/TriviaTaskController.java
+++ src/main/java/controller/TriviaTaskController.java
@@ -11,11 +11,12 @@ public class TriviaTaskController {
     public String passRandomQuestion(){
         return TriviaTaskUsecase.getRandomQuestion();
     }
+    public String passActualAnswer(){
+        return TriviaTaskUsecase.getActualAnswer();
+    }
 
-    // take the player's answer
-    // translate it into something the backend can understand
-    // relay it to the backend code and call the backend here
-    public boolean answerChecker(String actualAnswer, JTextField playerAnswer){
+// public boolean passAnswer
+    public boolean passAnswer(String actualAnswer, JTextField playerAnswer){
         // backend checkAnswer compares the playerAnswer to the actualAnswer to determine
         // if it is correct
         return (TriviaTaskUsecase.checkAnswer(actualAnswer, playerAnswer.getText()));
