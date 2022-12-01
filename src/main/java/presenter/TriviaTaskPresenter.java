a9aab957442c554c9925d9f79d8b2ac71543be9a
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-11-30 19:44:25 -0500

    started trivia task presenter

--- src/main/java/presenter/TriviaTaskPresenter.java
+++ src/main/java/presenter/TriviaTaskPresenter.java
@@ -1,4 +1,8 @@
 package presenter;
+import controller.TriviaTaskController;
+import usecases.TriviaTask;
 
 public class TriviaTaskPresenter {
+    // another thing for dependency inversion rule & clean architecture
+    // tells the view what it needs to prevent to the user
 }
