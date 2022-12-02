c9353cdc09c9589c085f6dc94e40107ad493af40
elenascobici <elena@scobici.com>
2022-12-01 20:28:48 -0500

    Fixed errors in GameMasterTest class.

--- src/main/java/usecases/Task.java
+++ src/main/java/usecases/Task.java
@@ -3,26 +3,6 @@ package usecases;
 import java.util.Hashtable;
 
 public abstract class Task {
-
-//    protected static boolean completed = false;
-//    protected static boolean activated = false;
-//
-//    public static void setCompletionStatus(boolean status) {
-//        completed = status;
-//    }
-//
-//    public static boolean getCompletionStatus() {
-//        return completed;
-//    }
-//
-//
-//    public static void setActivatedStatus(boolean status) {
-//        activated = status;
-//    }
-//
-//    public static boolean getActivatedStatus() {
-//        return activated;
-//    }
     protected static Hashtable<String, Boolean> completions = new Hashtable<String, Boolean>();
     protected static Hashtable<String, Boolean> activations = new Hashtable<String, Boolean>();
     public static void setCompletionStatus(String task, boolean status) {
