f04797a52e8f3912f8ec8112acdf3f41aeefc127
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-12-02 11:51:24 -0500

    edit

--- src/main/java/controller/TriviaTaskController.java
+++ src/main/java/controller/TriviaTaskController.java
@@ -5,11 +5,11 @@ import javax.swing.*;
 public class TriviaTaskController {
 //    private String playerAnswer;
 //    private String[] actualAnswer;
-    TriviaTask TriviaTaskUsecase = new TriviaTask();
+    TriviaTask triviaTaskUsecase = new TriviaTask();
 
     // passes the random question thru the controller from backend to view
     public String passRandomQuestion(){
-        return TriviaTaskUsecase.getRandomQuestion();
+        return triviaTaskUsecase.getRandomQuestion();
     }
 
     // take the player's answer
@@ -18,7 +18,7 @@ public class TriviaTaskController {
     public boolean answerChecker(String actualAnswer, JTextField playerAnswer){
         // backend checkAnswer compares the playerAnswer to the actualAnswer to determine
         // if it is correct
-        return (TriviaTaskUsecase.checkAnswer(actualAnswer, playerAnswer.getText()));
+        return (TriviaTask.checkAnswer(actualAnswer, playerAnswer.getText()));
     }
     public static void setCompletionStatus(boolean status) {
         TriviaTask.setCompletionStatus("TriviaTask", status);
