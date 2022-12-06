c51daa18eb22665ec769f5bd6551fb6a1f39e724
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-12-05 21:19:28 -0500

    added in trivia task view code from my scrappy branch

--- src/main/java/presenter/TriviaTaskPresenter.java
+++ src/main/java/presenter/TriviaTaskPresenter.java
@@ -1,18 +1,36 @@
 package presenter;
 
+import controller.TriviaTaskController;
+import usecases.TriviaTask;
+
+import static usecases.TriviaTask.*;
+
 /**
- * The trivia task presenter tells the view what to "present" to the user
- * Gets innformation from the use case
+ * The TriviaTask presenter is responsible for "telling the view what to present to the user"
+ * The presenter gets information from the usecase & uses that information to tell the view what to
+ * display/present to the user
  *
  * @author talia
  */
 
 public class TriviaTaskPresenter {
+
     public String tellViewQuestion(){
-        return "placeholder";
+
+        return TriviaTask.getRandomQuestion();
     }
-    public String tellViewifCorrect(){
-        return "abc placeholder";
+
+    public static String tellCorrectness(){
+        //if answer is correct
+        if (isAnswerCorrect){
+            // tell the view to present the word correct to the user
+            return "correct";
+        }
+        else{
+            // if not tell the view to present incorrect & what the correct answer would have
+            // been to the user
+            return "incorrect. correct answer is: " + actualAnswer;
+        }
+
     }
 }
-
