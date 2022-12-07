d1bb59af5a6bd70a1ed11153c84d2c5b2449926a
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-12-06 20:40:03 -0500

    added trivia questions

--- src/main/java/usecases/TriviaTask.java
+++ src/main/java/usecases/TriviaTask.java
@@ -19,6 +19,10 @@ public class TriviaTask extends Task {
         triviaDict.put("A violation of clean architecture is called *missing word* architecture", "dIrTy");
         triviaDict.put("The dude who wrote clean architecture is nicknamed ...", "unclE bOb");
         triviaDict.put("In what room are the csc207 lectures?", "hell");
+        triviaDict.put("REALLY BAD clean arc violations are called F***Y arc", "filthy");
+        triviaDict.put("what do you call code that totally disregards solid deign?", "liquid");
+        triviaDict.put("what is the 11th month of the year?", "november");
+        triviaDict.put("what is the capital of canada?", "ottawa");
         return triviaDict;
     }
     public static String getRandomQuestion(){
