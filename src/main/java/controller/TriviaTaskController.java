0dd94ae7039f1e995307a6840ae895e14306d7ac
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-12-08 17:03:08 -0500

    added javadocs to trivia task controller

--- src/main/java/controller/TriviaTaskController.java
+++ src/main/java/controller/TriviaTaskController.java
@@ -2,16 +2,23 @@ package controller;
 import usecases.TriviaTask;
 import javax.swing.*;
 
+/**
+ * The controller for the trivia task.
+ * Responsible for taking the user's input from the view, "translating" it into the appropriate datatype for the
+ * usecase.
+ * Acts as a "passer" between the view and the use case.
+ * @author talia
+ */
+
 public class TriviaTaskController {
+    /**
+     * Takes the user inputs passed by TriviaTaskView, and translates/packages then passes
+     * it to the usecase.
+     *
+     * @param playerAnswer the answer inputted by the user
+     */
 
-    TriviaTask TriviaTaskUsecase = new TriviaTask();
-
-    // take the player's answer
-    // translate it into something the backend can understand
-    // relay it to the backend code and call the backend here
     public static void passAnswer(JTextField playerAnswer){
-        // backend checkAnswer compares the playerAnswer to the actualAnswer to determine
-        // if it is correct
         TriviaTask.checkAnswer(playerAnswer.getText());
     }
     public static void setCompletionStatus(boolean status) {
