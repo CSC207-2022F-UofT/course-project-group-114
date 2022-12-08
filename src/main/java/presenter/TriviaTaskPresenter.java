8156027c96bb9fb6d12b488dccd580b159ddede9
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-12-08 17:09:53 -0500

    added javadocs to trivia task presenter changed the answer checking method in backend to a boolean

--- src/main/java/presenter/TriviaTaskPresenter.java
+++ src/main/java/presenter/TriviaTaskPresenter.java
@@ -15,21 +15,36 @@ import static usecases.TriviaTask.*;
 
 public class TriviaTaskPresenter {
 
+    /**
+     * Calls the usecase to get the random question and decides what update needs
+     * to be made to the view.
+     * @return String form of the random question that needs to be "presented" to the user.
+     */
     public static String tellViewQuestion(){
 
         return TriviaTask.getRandomQuestion();
     }
 
+    /**
+     * Finds out if the player answered the question correctly from the usecase, and decides what
+     * message the view needs to "present" to the user.
+     * @return String form of the message that needs to be displayed to the user to tell them if they answered
+     * the question correctly or not.
+     */
+
     public static String tellCorrectness(){
+        String correctnessMessage;
         //if answer is correct
         if (isAnswerCorrect){
             // tell the view to present the word correct to the user
-            return "correct";
+            correctnessMessage = "correct";
+            return correctnessMessage;
         }
         else{
             // if not tell the view to present incorrect & what the correct answer would have
             // been to the user
-            return "incorrect. correct answer is: " + actualAnswer;
+            correctnessMessage = "incorrect. correct answer is: " + actualAnswer;
+            return correctnessMessage;
         }
 
     }
