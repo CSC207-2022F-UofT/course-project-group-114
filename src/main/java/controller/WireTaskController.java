a3e1b722f5338764cca12bb20b06eb1e31498fe3
Roy Mao <roy.mao@mail.utoronto.ca>
2022-12-08 19:37:26 -0500

    WireTask Implementation Update

--- src/main/java/controller/WireTaskController.java
+++ src/main/java/controller/WireTaskController.java
@@ -8,19 +8,18 @@ import usecases.WireTask;
  */
 public class WireTaskController {
 
-    /**
-     * A setter method for the last two colours the user selected. If they are matching, remove them both from the list
-     */
-    public static boolean check_matching(String first, String second){
-        return WireTask.checkWiresSelected(first, second);
-        }
+//    /**
+//     * A setter method for the last two colours the user selected. If they are matching, remove them both from the list
+//     */
+//    public static boolean check_matching(String first, String second){
+//        return WireTask.checkWiresSelected(first, second);
+//        }
 
     /**
      * A setter method for WireTask's completion status.
      */
     public static void setCompletionStatus(boolean status) {
         WireTask.setCompletionStatus("WireTask", status);
-
     }
     public static void setActivationStatus(boolean status) {
 
